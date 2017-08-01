0f0b887bd7b027d122c810b60de13b8023d38862
KevinDepuydt <kevin.depuydt@supinternet.fr>
2017-08-01 16:39:16 +0200

    configure error page + router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// core
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+// users
+import { UserComponent } from './user/user.component';
+// auth
+import { LoginComponent } from './authentication/login/login.component';
+import { SubscribeComponent } from './authentication/subscribe/subscribe.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'subscribe', component: SubscribeComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    NotFoundComponent,
+    LoginComponent,
+    SubscribeComponent,
+    HomeComponent,
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
